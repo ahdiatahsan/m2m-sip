a70319668a0ea426639918735dc9a0b770f12f34
Ahdiat Ahsan <ahdiatahsanaa@gmail.com>
2024-01-11 16:57:45 +0800

    first commit

---
+++ app/Filament/Resources/TimeslotResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TimeslotResource\Pages;
+use App\Filament\Resources\TimeslotResource\RelationManagers;
+use App\Models\Timeslot;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TimeslotResource extends Resource
+{
+    protected static ?string $model = Timeslot::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTimeslots::route('/'),
+            'create' => Pages\CreateTimeslot::route('/create'),
+            'edit' => Pages\EditTimeslot::route('/{record}/edit'),
+        ];
+    }
+}
