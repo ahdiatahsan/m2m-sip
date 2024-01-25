de7e8bcbe92a2cfeae6ba9c4b2a32983390d9ab6
Ahdiat Ahsan <ahdiatahsanaa@gmail.com>
2024-01-25 10:51:46 +0800

    add teacher seeder and factory

---
+++ database/seeders/TeacherSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Teacher;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TeacherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $teacher = Teacher::factory()->create([
+            'name' => 'Teacher A',
+            'code' => 'A1',
+            'nip' => '123456789',
+            'phone' => '081212345678',
+            'address' => 'Makassar',
+            'user_id' => 3
+        ]);
+    }
+}
