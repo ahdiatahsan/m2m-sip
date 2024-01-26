806ffbcfe3f6c31fab320e1aee15348853799ad7
Ahdiat Ahsan <ahdiatahsanaa@gmail.com>
2024-01-26 10:12:41 +0800

    add default role assign for new user

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -3,10 +3,23 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Models\User;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function afterCreate(): void
+    {
+        $user = User::find($this->record->getKey());
+        $user->assignRole('admin');
+        // dd($user);
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
