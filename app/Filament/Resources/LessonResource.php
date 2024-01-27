1829069daa955b0d0afcc0db0954e3e0ba36814c
Ahdiat Ahsan <ahdiatahsanaa@gmail.com>
2024-01-27 14:22:16 +0800

    add basic crud for lesson

--- app/Filament/Resources/LessonResource.php
+++ app/Filament/Resources/LessonResource.php
@@ -6,9 +6,12 @@ use App\Filament\Resources\LessonResource\Pages;
 use App\Filament\Resources\LessonResource\RelationManagers;
 use App\Models\Lesson;
 use Filament\Forms;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -28,22 +31,39 @@ class LessonResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                //
-            ]);
+        ->schema([
+            Section::make()->schema([
+                TextInput::make('code')
+                    ->label('Kode')
+                    ->required()
+                    ->unique(ignoreRecord: true)
+                    ->autocomplete(false),
+                TextInput::make('name')
+                    ->label('Nama')
+                    ->required()
+                    ->autocomplete(false),
+            ])
+                ->columns(2),
+        ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('code')
+                    ->label('Kode')
+                    ->searchable(),
+                TextColumn::make('name')
+                    ->label('Nama Mata Pelajaran')
+                    ->searchable(),
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
