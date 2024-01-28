5893d92cec85e8bea53c348d59c0d6eefe0f32ec
Ahdiat Ahsan <ahdiatahsanaa@gmail.com>
2024-01-28 16:10:03 +0800

    add day seeder in db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ShieldSeeder::class,
             TeacherSeeder::class,
+            DaySeeder::class,
         ]);
     }
 }
